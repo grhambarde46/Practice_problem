2f11dcaa26fd0487ab3c682c00ed6f463f5f4f73
grhambarde46 <grhambarde46@gmail.com>
2022-11-16 14:52:09 +0530

    my first commit

---
+++ Practice_Program/src/com/java/Demo.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList arrayList = new ArrayList();
+		arrayList.add("Anil");
+		arrayList.add("Shiv");
+		arrayList.add("Gopal");
+		System.out.println(arrayList);
+
+	}
+
+}
